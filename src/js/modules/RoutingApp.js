b101e377833a6718611b7938303d3d263360cfe9
Jason Kao <jasonkao85@gmail.com>
2018-02-11 07:47:04 -0600

    routin

--- src/js/modules/RoutingApp.js
+++ src/js/modules/RoutingApp.js
@@ -5,7 +5,7 @@ import appHistory from "tools/appHistory";
 import ConnectedRouter from "react-router-redux/ConnectedRouter";
 import store from "../store";
 
-import { PageLayout, MainApp, Collection, Story } from "./core/components";
+import { PageLayout, MainApp, Collection, StoryCover, Story } from "./core/components";
 
 class RoutingApp extends Component {
   render() {
@@ -16,7 +16,8 @@ class RoutingApp extends Component {
             <Switch>
               <Route exact path="/" component={MainApp} />
               <Route exact path="/collection" component={Collection} />
-              <Route exact path="/stories/:story_id" component={Story} />
+              <Route exact path="/stories/:story_id" component={StoryCover} />
+              <Route exact path="/stories/:story_id/pages" component={Story} />
             </Switch>
           </PageLayout>
         </ConnectedRouter>
--- src/js/modules/core/reducer.js
+++ src/js/modules/core/reducer.js
@@ -1,7 +1,25 @@
-const initialState = {};
+import {
+  CHANGE_NAVBAR_TEXT
+} from "./actionTypes";
+
+const initialState = {
+  navbarText: ''
+};
 
 const reducer = (state = { ...initialState }, action) => {
   switch (action.type) {
+    case CHANGE_NAVBAR_TEXT: {
+      return {
+        ...state,
+        navbarText: action.payload,
+      };
+    }
+    case "@@router/LOCATION_CHANGE": {
+      return {
+        ...state,
+        navbarText: '',
+      };
+    }
   }
   return state;
 };
